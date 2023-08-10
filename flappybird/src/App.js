6ca9ca6411250c1ef084ef6bf5dc761c87b3a35b
Gurbeer-Sokhi <gsokhi@stevens.edu>
2023-08-10 15:05:57 -0400

    adding the flapping functionality

--- flappybird/src/App.js
+++ flappybird/src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 const WALL_HEIGHT = 600;
 const WALL_WIDTH = 400;
 const BIRD_WIDTH = 30;
 const BIRD_HEIGHT = 28;
+const GRAVITY = 5;
 function App() {
   const [birdpos, setBirdpos] = useState(300);
+
+  useEffect(() => {
+    let birdval;
+    if (birdpos < WALL_HEIGHT - BIRD_HEIGHT) {
+      birdval = setInterval(() => {
+        setBirdpos((birdpos) => birdpos + GRAVITY);
+      }, 24);
+    }
+    return () => clearInterval(birdval);
+  });
+
+  const handler = () => {
+    if (birdpos < BIRD_HEIGHT) {
+      setBirdpos(0);
+    } else {
+      setBirdpos((birdpos) => birdpos - 50);
+    }
+  };
   return (
-    <Home>
+    <Home onClick={handler}>
       <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
+        <Startgame>Click To Start Game</Startgame>
         <Bird
           height={BIRD_HEIGHT}
           width={BIRD_WIDTH}
@@ -47,4 +67,18 @@ const Bird = styled.div`
   height: ${(props) => props.height}px;
   top: ${(props) => props.top}px;
   left: ${(props) => props.left}px;
+`;
+
+const Startgame = styled.div`
+  text-align: center;
+  position: relative;
+  top: 49%;
+  background-color: black;
+  padding: 10px;
+  width: 100px;
+  left: 50%;
+  margin-left: -55px;
+  font-size: 20px;
+  border-radius: 10px;
+  color: white;
 `;
